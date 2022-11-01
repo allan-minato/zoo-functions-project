903592c4f737c0e5a61317adcf5664e5012c05ee
Allan Medeiros Minato <allan.minato@gmail.com>
2022-11-01 20:56:33 -0300

    Resolução exercício 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,15 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  return employees.find(
+    (employeed) =>
+      employeeName === employeed.firstName
+      || employeeName === employeed.lastName,
+  );
 }
 
 module.exports = getEmployeeByName;
