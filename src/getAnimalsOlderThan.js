8f97494b85e6fde381ea133b16eb3d0d1318c905
Allan Medeiros Minato <allan.minato@gmail.com>
2022-11-01 19:35:55 -0300

    Resolução do exercício 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
-const data = require('../data/zoo_data');
+const { species } = require("../data/zoo_data");
+const data = require("../data/zoo_data");
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return species
+    .find((specie) => specie.name === animal)
+    .residents.every((minAge) => minAge.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
