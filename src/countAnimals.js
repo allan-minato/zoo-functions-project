fb90d63a8dbf1bead7bb2599e53558cd233ddb77
Allan Medeiros Minato <allan.minato@gmail.com>
2022-11-03 15:54:28 -0300

    Resolução exercício 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    const newObject = {};
+    species.forEach((specie) => {
+      newObject[specie.name] = specie.residents.length;
+    });
+    return newObject;
+  }
+  if (animal.specie && animal.sex) {
+    return species
+      .find((animals) => animal.specie === animals.name)
+      .residents.filter((sex) => animal.sex === sex.sex).length;
+  }
+  if (animal.specie) {
+    return species.find((animals) => animal.specie === animals.name).residents
+      .length;
+  }
 }
 
 module.exports = countAnimals;
