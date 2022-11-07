dffaf0b86d8dcedec7f3758afafae0a8bd9506d9
Allan Medeiros Minato <allan.minato@gmail.com>
2022-11-06 23:33:50 -0300

    Resolução exercício 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,27 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter((entrant) => entrant.age < 18);
+  const adult = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50);
+  const senior = entrants.filter((entrant) => entrant.age >= 50);
+  const newObject = {
+    child: child.length,
+    adult: adult.length,
+    senior: senior.length,
+  };
+
+  return newObject;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || !entrants.length) {
+    return 0;
+  }
+  const valueChild = countEntrants(entrants).child * data.prices.child;
+  const valueAdult = countEntrants(entrants).adult * data.prices.adult;
+  const valueSenior = countEntrants(entrants).senior * data.prices.senior;
+
+  return valueChild + valueAdult + valueSenior;
 }
 
 module.exports = { calculateEntry, countEntrants };
