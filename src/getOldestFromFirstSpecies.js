abd5f99649000d7fc1dabe72d1b319e64652af12
Allan Medeiros Minato <allan.minato@gmail.com>
2022-11-07 03:48:29 -0300

    Resolução exercício 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
+const { employees, species } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const getEmploy = employees.find((employ) => id === employ.id).responsibleFor[0];
+  const getAnimal = species.find((specie) => specie.id === getEmploy);
+  const olderAnimal = getAnimal.residents.sort((a, b) => b.age - a.age);
+  return Object.values(olderAnimal[0]);
 }
 
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  if (scheduleTarget === undefined) {
+    return data.species
+      .filter((specie) => specie.availability.includes(scheduleTarget))
+      .map((specie) => specie.name);
+  }
+  const nameSpecie = data.species.filter((specie) => specie.name === scheduleTarget);
+  return nameSpecie.map((specie) => specie.availability).find((specie) => specie);
 }
+
+console.log(getSchedule());
 
 module.exports = getSchedule;
