71a676ae0691ab2a6cb6ffad18dee46568352cdd
Allan Medeiros Minato <allan.minato@gmail.com>
2022-11-09 02:30:07 -0300

    Resolução exercício 9

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,15 +1,61 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  if (scheduleTarget === undefined) {
-    return data.species
-      .filter((specie) => specie.availability.includes(scheduleTarget))
-      .map((specie) => specie.name);
-  }
-  const nameSpecie = data.species.filter((specie) => specie.name === scheduleTarget);
-  return nameSpecie.map((specie) => specie.availability).find((specie) => specie);
+const daysOfWeek = Object.keys(data.hours);
+const names = data.species.map((specie) => specie.name);
+
+function nameOfAnimal(name) {
+  return names.includes(name);
 }
 
-console.log(getSchedule());
+function getAnimal(name) {
+  const { availability } = data.species.find((specie) => specie.name === name);
+  return availability;
+}
 
+function dayOfTheWeek(day) {
+  return daysOfWeek.includes(day);
+}
+
+function formatOfficeHour({ open, close }) {
+  return `Open from ${open}am until ${close}pm`;
+}
+
+function getDay(day) {
+  const result = {};
+  let exhibition;
+  let officeHour;
+
+  if (day === 'Monday') {
+    exhibition = 'The zoo will be closed!';
+    officeHour = 'CLOSED';
+  } else {
+    officeHour = formatOfficeHour(data.hours[day]);
+    exhibition = data.species
+      .filter((specie) => specie.availability.includes(day))
+      .map((specie) => specie.name);
+  }
+
+  result[day] = { officeHour, exhibition };
+
+  return result;
+}
+
+function getAll() {
+  let result = {};
+  daysOfWeek.forEach((day) => {
+    result = Object.assign(getDay(day), result);
+  });
+
+  return result;
+}
+
+function getSchedule(scheduleTarget) {
+  if (nameOfAnimal(scheduleTarget)) {
+    return getAnimal(scheduleTarget);
+  }
+  if (dayOfTheWeek(scheduleTarget)) {
+    return getDay(scheduleTarget);
+  }
+  return getAll();
+}
 module.exports = getSchedule;
