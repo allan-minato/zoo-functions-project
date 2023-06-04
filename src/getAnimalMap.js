c2cadce5b7316d8f1ad2a6bfb14ee021b150de0c
Allan Medeiros Minato <allan.minato@gmail.com>
2023-06-04 14:28:45 -0300

    fix cli

--- src/getAnimalMap.js
+++
@@ -1,7 +0,0 @@
-const data = require('../data/zoo_data');
-
-function getAnimalMap(options) {
-  // seu código aqui
-}
-
-module.exports = getAnimalMap;
--- src/getEmployeesCoverage.js
+++
@@ -1,7 +0,0 @@
-const data = require('../data/zoo_data');
-
-function getEmployeesCoverage() {
-  // seu código aqui
-}
-
-module.exports = getEmployeesCoverage;
